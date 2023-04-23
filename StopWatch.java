7dc34ead2120cbb2d5db061ee5183644ff2b13ba
SCP-714 <113054208+SCP-714@users.noreply.github.com>
2023-04-22 23:43:47 -0500

    created framework and global variables

---
+++ StopWatch.java
@@ -0,0 +1,32 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class StopWatch implements ActionListener {
+
+    JFrame frame = new JFrame();
+    JButton startButton = new JButton("Start");
+    JButton resetButton = new JButton("Reset");
+    JLabel timeLabel = new JLabel();
+    int elapsedTime = 0;
+    StopWatch(){
+
+    }
+    @overide
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    void start() {
+
+    }
+
+    void stop() {
+
+    }
+
+    void reset(){
+
+    }
+
+}
