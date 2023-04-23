432fdb54309660c483332c3ca707a989d715cae7
SCP-714 <113054208+SCP-714@users.noreply.github.com>
2023-04-23 12:38:36 -0500

    displays screen properly

--- StopWatch.java
+++ StopWatch.java
@@ -16,7 +16,45 @@ public class StopWatch implements ActionListener {
     String seconds_string = String.format("%02d", seconds);
     String minutes_string = String.format("%02d", minutes);
     String hours_string = String.format("%02d", hours);
+
+    Timer timer = new Timer(1000, new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+            elapsedTime+=1000;
+            hours = (elapsedTime/3600000);
+            minutes = (elapsedTime/60000) % 60;
+            seconds = (elapsedTime/1000) % 60;
+            seconds_string = String.format("%02d", seconds);
+            minutes_string = String.format("%02d", minutes);
+            hours_string = String.format("%02d", hours);
+            timeLabel.setText(hours_string + ":" + minutes_string + ":" + seconds_string);
+
+        }
+    });
+
     StopWatch(){
+
+        timeLabel.setText(hours_string + ":" + minutes_string + ":" + seconds_string);
+        timeLabel.setBounds(100,100,200,100);
+        timeLabel.setFont(new Font("Verdana", Font.PLAIN, 40));
+        timeLabel.setBorder(BorderFactory.createBevelBorder(1));
+        timeLabel.setOpaque(true);
+        timeLabel.setHorizontalAlignment(JTextField.CENTER);
+
+        startButton.setBounds(100,200,100,50);
+        startButton.setFont(new Font("Ink Free", Font.PLAIN, 20));
+        startButton.setFocusable(true);
+        startButton.addActionListener(this);
+
+        resetButton.setBounds(200,200,100,50);
+        resetButton.setFont(new Font("Ink Free", Font.PLAIN, 20));
+        resetButton.setFocusable(true);
+        resetButton.addActionListener(this);
+
+        frame.add(startButton);
+        frame.add(resetButton);
+        frame.add(timeLabel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(425,425);
         frame.setLayout(null);
